34b008d9a0f6e316a13bd34d5a02c3c552b74d03
Hanx <xuanxinhan@vip.qq.com>
2019-02-12 17:27:00 +0800

    chore: update to 2.6.6

--- src/shared/constants.js
+++ src/shared/constants.js
@@ -20,5 +20,6 @@ export const LIFECYCLE_HOOKS = [
   'destroyed',
   'activated',
   'deactivated',
-  'errorCaptured'
+  'errorCaptured',
+  'serverPrefetch' // 2.6 新增的钩子 参考这个 PR: https://github.com/vuejs/vue/pull/9017
 ]
