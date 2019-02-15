9596c1923cf40ef0d636fadbe010fc991a396400
Hanx <xuanxinhan@vip.qq.com>
2019-02-15 17:18:18 +0800

    chore: how to oberve an array

---
+++ src/core/observer/array.js
@@ -0,0 +1,53 @@
+/*
+ * not type checking this file because flow doesn't play well with
+ * dynamically accessing methods on Array prototype
+ */
+
+import { def } from '../util/index'
+
+const arrayProto = Array.prototype
+// 拷贝了一份 Array.prototype 来重写方法，避免干扰原来的 Array.prototype
+export const arrayMethods = Object.create(arrayProto)
+
+// 要重写的方法
+const methodsToPatch = [
+  'push',
+  'pop',
+  'shift',
+  'unshift',
+  'splice',
+  'sort',
+  'reverse'
+]
+
+/**
+ * Intercept mutating methods and emit events
+ */
+methodsToPatch.forEach(function (method) {
+  // cache original method
+  // 缓存原方法
+  const original = arrayProto[method]
+  def(arrayMethods, method, function mutator (...args) {
+    // 调用原方法拿到结果，最终再返回
+    const result = original.apply(this, args)
+    const ob = this.__ob__
+    let inserted
+    // push, unshift, splice 都是插入数组元素的操作
+    // inserted 拿到插入的元素或者元素集合
+    switch (method) {
+      case 'push':
+      case 'unshift':
+        inserted = args
+        break
+      case 'splice':
+        inserted = args.slice(2)
+        break
+    }
+    // 插入的元素需要调用观测数组的方法完成观测
+    if (inserted) ob.observeArray(inserted)
+    // notify change
+    // 通知依赖更新
+    ob.dep.notify()
+    return result
+  })
+})
